9adc439cb4c5a7b5307ab8d0dd9891b105cd1adf
Gamaliel Bernal <gamalielbj@gmail.com>
2021-08-09 19:50:26 -0500

    Fixed the edit feature

--- src/components/views/Details/index.js
+++ src/components/views/Details/index.js
@@ -3,8 +3,8 @@ import { useParams } from 'react-router-dom';
 
 import { useDispatch } from 'react-redux';
 import {
-  addCurrentCharacterAction,
-  editCharacterAction,
+    addCurrentCharacterAction,
+    editCharacterAction,
 } from '../../../redux/actions/rickAndMortyAction';
 
 import { Row, Input, Button } from 'antd';
@@ -12,106 +12,112 @@ import { Row, Input, Button } from 'antd';
 import './Details.scss';
 
 const Details = () => {
-  const [details, setDetails] = useState(null);
-  const [location, setLocation] = useState('');
-  const [gender, setGender] = useState('');
-  const [specie, setSpecie] = useState('');
-  const [origin, setOrigin] = useState('');
-  const [status, setStatus] = useState('');
+    const [details, setDetails] = useState(null);
+    const [location, setLocation] = useState('');
+    const [gender, setGender] = useState('');
+    const [specie, setSpecie] = useState('');
+    const [origin, setOrigin] = useState('');
+    const [status, setStatus] = useState('');
 
-  const axios = require('axios');
-  const dispatch = useDispatch();
-  const params = useParams();
+    const axios = require('axios');
+    const dispatch = useDispatch();
+    const params = useParams();
 
-  useEffect(() => {
-    fetchDetails(params.id);
-    return () => {
-      setDetails(null);
+    useEffect(() => {
+        fetchDetails(params.id);
+        return () => {
+            setDetails(null);
+        };
+    }, [params.id]);
+
+    useEffect(() => {
+        details !== null && dispatch(addCurrentCharacterAction(details));
+    }, [details]);
+
+    const fetchDetails = async (id) => {
+        const response = await axios.get(
+            `${process.env.REACT_APP_AXIOS_URL}/${id}`
+        );
+        setDetails(response.data);
     };
-  }, [params.id]);
 
-  useEffect(() => {
-    details !== null && dispatch(addCurrentCharacterAction(details));
-  }, [details]);
+    const handleSave = () => {
+        const currentCopy = { ...details };
+        currentCopy.gender = (gender !== '' && gender) || currentCopy.gender;
+        currentCopy.location =
+            (location !== '' && { name: location }) || currentCopy.location;
+        currentCopy.origin =
+            (origin !== '' && { name: origin }) || currentCopy.origin;
+        currentCopy.species = (specie !== '' && specie) || currentCopy.species;
+        currentCopy.status = (status !== '' && status) || currentCopy.status;
 
-  const fetchDetails = async (id) => {
-    const response = await axios.get(
-      `${process.env.REACT_APP_AXIOS_URL}/${id}`
+        dispatch(editCharacterAction(currentCopy));
+    };
+
+    return (
+        <Row className='detailsContainer'>
+            <Row
+                style={{
+                    width: '100%',
+                    height: '30%',
+                    justifyContent: 'center',
+                }}>
+                <img src={details?.image} alt={details?.name} />
+            </Row>
+            {Object.values(params).length > 1 ? (
+                <Row className='detailsContainer__information'>
+                    <div className='title'>{details?.name}</div>
+                    <div className='items'>
+                        Ubicación
+                        <Input
+                            placeholder={details?.location?.name}
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
+                    </div>
+                    <div className='items'>
+                        Género
+                        <Input
+                            placeholder={details?.gender}
+                            onChange={(e) => setGender(e.target.value)}
+                        />
+                    </div>
+                    <div className='items'>
+                        Especie{' '}
+                        <Input
+                            placeholder={details?.species}
+                            onChange={(e) => setSpecie(e.target.value)}
+                        />
+                    </div>
+                    <div className='items'>
+                        Origen{' '}
+                        <Input
+                            placeholder={details?.origin?.name}
+                            onChange={(e) => setOrigin(e.target.value)}
+                        />
+                    </div>
+                    <div className='items'>
+                        Estado
+                        <Input
+                            placeholder={details?.status}
+                            onChange={(e) => setStatus(e.target.value)}
+                        />
+                    </div>
+                    <Button type='primary' onClick={handleSave}>
+                        Guardar
+                    </Button>
+                </Row>
+            ) : (
+                <Row className='detailsContainer__information'>
+                    <div className='title'>{details?.name}</div>
+                    <div>Ubicación: {details?.location?.name}</div>
+                    <div>Género: {details?.gender}</div>
+                    <div>Especie: {details?.species}</div>
+                    <div>Origen: {details?.origin?.name}</div>
+                    <div>Estado: {details?.status}</div>
+                </Row>
+            )}
+        </Row>
     );
-    setDetails(response.data);
-  };
-
-  const handleSave = () => {
-    const currentCopy = { ...details };
-    currentCopy.gender = (gender !== '' && gender) || currentCopy.gender;
-    currentCopy.location =
-      (location !== '' && { name: location }) || currentCopy.location;
-    currentCopy.origin =
-      (origin !== '' && { name: origin }) || currentCopy.origin;
-    currentCopy.species = (specie !== '' && specie) || currentCopy.species;
-    currentCopy.status = (status !== '' && status) || currentCopy.status;
-    dispatch(editCharacterAction(currentCopy));
-  };
-
-  return (
-    <Row className='detailsContainer'>
-      <Row style={{ width: '100%', height: '30%', justifyContent: 'center' }}>
-        <img src={details?.image} alt={details?.name} />
-      </Row>
-      {Object.values(params).length > 1 ? (
-        <Row className='detailsContainer__information'>
-          <div className='title'>{details?.name}</div>
-          <div className='items'>
-            Ubicación
-            <Input
-              placeholder={details?.location?.name}
-              onChange={(e) => setLocation(e.target.value)}
-            />
-          </div>
-          <div className='items'>
-            Género
-            <Input
-              placeholder={details?.gender}
-              onChange={(e) => setGender(e.target.value)}
-            />
-          </div>
-          <div className='items'>
-            Especie{' '}
-            <Input
-              placeholder={details?.species}
-              onChange={(e) => setSpecie(e.target.value)}
-            />
-          </div>
-          <div className='items'>
-            Origen{' '}
-            <Input
-              placeholder={details?.origin?.name}
-              onChange={(e) => setOrigin(e.target.value)}
-            />
-          </div>
-          <div className='items'>
-            Estado
-            <Input
-              placeholder={details?.status}
-              onChange={(e) => setStatus(e.target.value)}
-            />
-          </div>
-          <Button type='primary' onClick={handleSave}>
-            Guardar
-          </Button>
-        </Row>
-      ) : (
-        <Row className='detailsContainer__information'>
-          <div className='title'>{details?.name}</div>
-          <div>Ubicación: {details?.location?.name}</div>
-          <div>Género: {details?.gender}</div>
-          <div>Especie: {details?.species}</div>
-          <div>Origen: {details?.origin?.name}</div>
-          <div>Estado: {details?.status}</div>
-        </Row>
-      )}
-    </Row>
-  );
 };
 
 export default Details;
--- src/redux/reducers/rickAndMortyReducer.js
+++ src/redux/reducers/rickAndMortyReducer.js
@@ -1,60 +1,61 @@
 import {
-  ADD_CHARACTER,
-  DELETE_CHARACTER,
-  EDIT_CHARACTER,
-  ADD_CURRENT_CHARACTER,
-  FETCH_CHARACTERS,
+    ADD_CHARACTER,
+    DELETE_CHARACTER,
+    EDIT_CHARACTER,
+    ADD_CURRENT_CHARACTER,
+    FETCH_CHARACTERS,
 } from '../Constants';
 
 const initialState = {
-  characters: [],
-  currentCharacter: {},
+    characters: [],
+    currentCharacter: {},
 };
 
 const rickAndMortyReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_CHARACTERS:
-      return {
-        ...state,
-        characters: action.payload,
-      };
-    case ADD_CHARACTER:
-      const copy = [...state.characters, action.payload];
-      return {
-        ...state,
-        characters: copy,
-      };
+    switch (action.type) {
+        case FETCH_CHARACTERS:
+            return {
+                ...state,
+                characters: action.payload,
+            };
+        case ADD_CHARACTER:
+            const copy = [...state.characters, action.payload];
+            return {
+                ...state,
+                characters: copy,
+            };
 
-    case EDIT_CHARACTER:
-      const charactersCopy = [...state.characters];
-      const copy2 = charactersCopy.filter(
-        (character) => character.id !== state.currentCharacter.id
-      );
-      return {
-        ...state,
-        characters: [...copy2, action.payload],
-      };
+        case EDIT_CHARACTER:
+            return {
+                ...state,
+                characters: state.characters.map((character) => {
+                    if (character.id === action.payload.id) {
+                        return { ...action.payload };
+                    }
+                    return character;
+                }),
+            };
 
-      break;
-    case DELETE_CHARACTER:
-      const copy3 = [...state.characters];
-      const updatedCharacters = copy3.filter(
-        (character) => character.id !== action.payload
-      );
-      return {
-        ...state,
-        characters: updatedCharacters,
-      };
-      break;
-    case ADD_CURRENT_CHARACTER:
-      return {
-        ...state,
-        currentCharacter: action.payload,
-      };
-      break;
-    default:
-      return state;
-  }
+            break;
+        case DELETE_CHARACTER:
+            const copy3 = [...state.characters];
+            const updatedCharacters = copy3.filter(
+                (character) => character.id !== action.payload
+            );
+            return {
+                ...state,
+                characters: updatedCharacters,
+            };
+            break;
+        case ADD_CURRENT_CHARACTER:
+            return {
+                ...state,
+                currentCharacter: action.payload,
+            };
+            break;
+        default:
+            return state;
+    }
 };
 
 export default rickAndMortyReducer;
